bfeec51e5ceaa542a8c058ca363bab57b67959ad
vikharunnisa <vikhar.unnisa@gmail.com>
2017-04-14 16:12:48 +0530

    Add files via upload

---
+++ testingHost.user.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name        testingHost
+// @namespace   https://investigator-workbench-uk.amazon.com/
+// @include     https://investigator-workbench-uk.amazon.com/*
+// @version     1
+// @grant       none
+// ==/UserScript==
+alert("version 1!");
